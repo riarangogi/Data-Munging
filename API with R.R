cf184e29710f83d80957c566bb204843ddcf2ecf
riarangogi <riarangogi@gmail.com>
2017-09-22 18:06:44 -0500

    This is a guide to enter any API using R.

---
+++ API with R.R
@@ -0,0 +1,5 @@
+#Guide to enter the github API's with R using htrr package
+#First: To regist the new application in https://github.com/settings/developers
+#Use any URL for the homepage URL (http://github.com is fine)
+#and http://localhost:1410 as the callback url
+
