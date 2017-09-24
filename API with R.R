2af75b4e944d7c1fd2942ab0377b1e6476b6182d
riarangogi <riarangogi@gmail.com>
2017-09-24 13:05:29 -0500

    The first API explained is github API.

--- API with R.R
+++ API with R.R
@@ -1,4 +1,7 @@
-#Guide to enter the github API's with R using htrr package
+library(httr)#previously this package should be installed
+
+
+#Guide to enter the github API's with R using httr package
 #First: To regist the new application in https://github.com/settings/developers
 #Use any URL for the homepage URL (http://github.com is fine)
 #and http://localhost:1410 as the callback url
