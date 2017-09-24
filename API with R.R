53f5146c26b2c08d46df90ea28129def492ba09c
riarangogi <riarangogi@gmail.com>
2017-09-24 16:10:37 -0500

    The profiles application is to get a visulization of profiles..

--- API with R.R
+++ API with R.R
@@ -6,3 +6,14 @@ library(httr)#previously this package should be installed
 #Use any URL for the homepage URL (http://github.com is fine)
 #and http://localhost:1410 as the callback url
 
+oauth_endpoints("github")#We get some specific functions to that API we want
+#enter. In this case 'github'
+
+myapp<-oauth_app("profiles",key = '5c576e360156527b85f9',
+                 secret = 'd90ff04522d2c38d3bf900bda5964bb8d0058fc2')
+#The arguments are appname,client id y client secret respectly, we get these 
+#when we create the app in the API
+
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+#we save in req the code from url
